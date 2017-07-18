5041c3b2af0894b0170f8136fa742339d2489086
Eric Menendez <ericmenendez@gmail.com>
2017-07-18 10:10:46 -0700

    Add k8s current context's default namespace to prompt.

--- segments/k8scontext.py
+++ segments/k8scontext.py
@@ -1,11 +1,19 @@
+import json
 import subprocess
 
-def add_k8scontext_segment(powerline):    
+def add_k8scontext_segment(powerline):
     try:
-        p = subprocess.Popen(['kubectl', 'config', 'current-context'],
+        p = subprocess.Popen(['kubectl', 'config', 'view', '-o', 'json'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = p.communicate()[0].decode("utf-8").rstrip('\n')
+        output = p.communicate()[0].decode("utf-8")
         if p.returncode == 0:
-            powerline.append(' %s ' % output, Color.K8SCONTEXT_FG, Color.K8SCONTEXT_BG)
+            config = json.loads(output)
+            current_context = config['current-context']
+            for context in config['contexts']:
+                if context['name'] == current_context:
+                    namespace = context['context'].get('namespace', 'default')
+                    break
+            powerline.append(' %s.%s ' % (current_context, namespace),
+                             Color.K8SCONTEXT_FG, Color.K8SCONTEXT_BG)
     except Exception:
         pass
