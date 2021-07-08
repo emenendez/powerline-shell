a97af2e1221c50d02d44768fe538069d3f54ea8f
Eric Menendez <emenendez@twitter.com>
2021-07-07 18:09:17 -0600

    Update my changes for latest powerline

--- powerline_shell/segments/k8scontext.py
+++ powerline_shell/segments/k8scontext.py
@@ -1,19 +1,24 @@
 import json
 import subprocess
 
-def add_k8scontext_segment(powerline):
-    try:
-        p = subprocess.Popen(['kubectl', 'config', 'view', '-o', 'json'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = p.communicate()[0].decode("utf-8")
-        if p.returncode == 0:
-            config = json.loads(output)
-            current_context = config['current-context']
-            for context in config['contexts']:
-                if context['name'] == current_context:
-                    namespace = context['context'].get('namespace', 'default')
-                    break
-            powerline.append(' %s.%s ' % (current_context, namespace),
-                             Color.K8SCONTEXT_FG, Color.K8SCONTEXT_BG)
-    except Exception:
-        pass
+from ..utils import BasicSegment, warn
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        try:
+            p = subprocess.Popen(['kubectl', 'config', 'view', '-o', 'json'],
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = p.communicate()[0].decode("utf-8")
+            if p.returncode == 0:
+                config = json.loads(output)
+                current_context = config['current-context']
+                for context in config['contexts']:
+                    if context['name'] == current_context:
+                        namespace = context['context'].get('namespace', 'default')
+                        break
+                self.powerline.append(' %s.%s ' % (current_context, namespace),
+                                      self.powerline.theme.AWS_PROFILE_FG,
+                                      self.powerline.theme.AWS_PROFILE_BG)
+        except Exception:
+            pass
--- powerline_shell/segments/stdout.py
+++ powerline_shell/segments/stdout.py
@@ -5,13 +5,15 @@ from ..utils import ThreadedSegment
 class Segment(ThreadedSegment):
     def run(self):
         cmd = self.segment_def["command"]
-        self.output = subprocess.check_output(cmd).decode("utf-8").strip()
-        # TODO handle OSError
-        # TODO handle no command defined or malformed
+        try:
+            self.output = subprocess.check_output(cmd, stderr=subprocess.PIPE).decode("utf-8").strip()
+        except Exception:
+            self.output = None
 
     def add_to_powerline(self):
         self.join()
-        self.powerline.append(
-            " %s " % self.output,
-            self.segment_def.get("fg_color", self.powerline.theme.PATH_FG),
-            self.segment_def.get("bg_color", self.powerline.theme.PATH_BG))
+        if self.output:
+            self.powerline.append(
+                " %s " % self.output,
+                self.segment_def.get("fg_color", self.powerline.theme.PATH_FG),
+                self.segment_def.get("bg_color", self.powerline.theme.PATH_BG))
